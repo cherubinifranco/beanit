38e5d483b095fbecefcea6eee3a61e8c863ceb7e
cherubinifranco <cherubini.franco@hotmail.com>
2023-10-04 20:57:16 -0300

    Fixed how sendMails function works

--- public/main.js
+++ public/main.js
@@ -4,7 +4,6 @@ const path = require("path");
 const { sendReportsToClients, sendMailsToClients } = require("./mailSender");
 const { fetchDataFromXLSX, fetchSampleDataFromXLSX } = require("./fetchData");
 
-
 function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
@@ -25,7 +24,7 @@ function createWindow() {
     : "http://localhost:3000/";
 
   win.loadURL(windowURL);
-  ipcMain.on('closeApp', () => {
+  ipcMain.on("closeApp", () => {
     win.close();
   });
   ipcMain.on("maxResApp", () => {
@@ -40,63 +39,58 @@ function createWindow() {
   });
 
   ipcMain.on("showDialog", async (event, dialogInfo) => {
-    dialog.showMessageBox(win, dialogInfo)
+    dialog.showMessageBox(win, dialogInfo);
   });
 
-  ipcMain.handle('selectDirectory', async function() {
-
+  ipcMain.handle("selectDirectory", async function () {
     let dir = await dialog.showOpenDialog(win, {
-  
-        properties: ['openDirectory']
-  
+      properties: ["openDirectory"],
     });
 
-    return dir.filePaths[0]
-  
+    return dir.filePaths[0];
   });
 
-  ipcMain.handle('selectFile', async function() {
-
+  ipcMain.handle("selectFile", async function () {
     let file = await dialog.showOpenDialog(win, {
-  
-        properties: ['openFile']
-  
+      properties: ["openFile"],
     });
 
-    return file.filePaths[0]
-  
+    return file.filePaths[0];
   });
 
-  ipcMain.handle('getSampleData', async (event, xlsxFile) =>{
+  ipcMain.handle("getSampleData", async (event, xlsxFile) => {
     const sampleData = await fetchSampleDataFromXLSX(xlsxFile);
     return sampleData;
-  })
+  });
 
-  ipcMain.handle('fetchAllData', async (event, xlsxFile) =>{
+  ipcMain.handle("fetchAllData", async (event, xlsxFile) => {
     const data = await fetchDataFromXLSX(xlsxFile);
     return data;
-  })
+  });
 
-  ipcMain.handle('sendMails', async (event, mailInfo) =>{
+  ipcMain.handle("sendMails", async (event, mailInfo) => {
     const clientData = await fetchDataFromXLSX(mailInfo.xlsxFile);
-    const temp = await win.webContents.executeJavaScript(
+    let temp = await win.webContents.executeJavaScript(
       'localStorage.getItem("ownVariables");',
       true
-    )
-    const ownVariables = {}
-    const ownVariablesLS = JSON.parse(temp)
-    ownVariablesLS.forEach(el => ownVariables[el[0]] = el[1])
-    const data = await sendMailsToClients(clientData, mailInfo, ownVariables)
+    );
+    if (temp == undefined) temp = [];
+    const ownVariables = {};
+    const ownVariablesLS = JSON.parse(temp);
+    ownVariablesLS.forEach((el) => (ownVariables[el[0]] = el[1]));
+    const data = await sendMailsToClients(clientData, mailInfo, ownVariables);
     return data;
-  })
+  });
 
-  ipcMain.handle('sendTicket', async (event, mailInfo) =>{
-    const supportData = [{
-      mail: "cherubini.franco@hotmail.com"
-    }]
-    const data = await sendMailsToClients(supportData, mailInfo)
+  ipcMain.handle("sendTicket", async (event, mailInfo) => {
+    const supportData = [
+      {
+        mail: "cherubini.franco@hotmail.com",
+      },
+    ];
+    const data = await sendMailsToClients(supportData, mailInfo);
     return data; // [errores, sendedMails]
-  })
+  });
 }
 
 app.whenReady().then(() => {
@@ -106,6 +100,3 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
-
-
-
